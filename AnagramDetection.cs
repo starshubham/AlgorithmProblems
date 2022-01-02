15904e23aca8497ad96c8e53ae2332989b4dd410
Shubham Seth <shubhamsth1234@gmail.com>
2022-01-02 15:53:42 +0530

    Added Anagram detection program

---
+++ AnagramDetection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+/*
+An Anagram Detection Example
+a. Desc -> One string is an anagram of another if the second is simply a
+rearrangement of the first. For example, 'heart' and 'earth' are anagrams...
+b. I/P -> Take 2 Strings as Input such abcd and dcba and Check for Anagrams
+c. O/P -> The Two Strings are Anagram or not...
+ */
+
+namespace AnagramDetectionAndPrimeNumber
+{
+    public class AnagramDetection
+    {  
+        public bool areAnagram(string firstString, string secondString)
+        {
+            if (firstString.Length != secondString.Length)
+            {
+                return false;
+            }
+            //Convert string to character array  
+            char[] firstCharsArray = firstString.ToLower().ToCharArray();
+            char[] secondCharsArray = secondString.ToLower().ToCharArray();
+            //Sort array  
+            Array.Sort(firstCharsArray);
+            Array.Sort(secondCharsArray);
+            //Check each character and position.  
+            for (int i = 0; i < firstCharsArray.Length; i++)
+            {
+                if (firstCharsArray[i].ToString() != secondCharsArray[i].ToString())
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ MainProgForAnagranAndPrimeNo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AnagramDetectionAndPrimeNumber
+{
+    internal class MainProgForAnagranAndPrimeNo
+    {
+        static void Main(string[] args)
+        {
+            // Q1. Anagram Detection
+            string firstString, secondString;
+            //Gets words from user.  
+            Console.WriteLine("Enter first string");
+            firstString = Console.ReadLine();
+            Console.WriteLine("Enter second string");
+            secondString = Console.ReadLine();
+            AnagramDetection anagram = new AnagramDetection();
+            //Check if words are anagram  
+            if (anagram.areAnagram(firstString, secondString) == true)
+            {
+                Console.WriteLine("Both string are anagram string.");
+            }
+            else
+            {
+                Console.WriteLine("Both string are not anagram string.");
+            }
+            Console.WriteLine("\n***********End of Question1***********\n");
+
+
+            // Q2. Find Prime number in Range
+            PrimeNoInRange primeRange = new PrimeNoInRange();
+            Console.WriteLine("Find Prime numbers between given range : ");
+            primeRange.PrintPrime();
+            Console.WriteLine("\n***********End of Question2***********\n");
+
+
+            // Q3. find the prime numbers that are Anagram and Palindrome
+            CheckPrimePalindromeAndAnagram obj = new CheckPrimePalindromeAndAnagram();
+            Console.WriteLine("Prime numbers that are Anagram and Palindrome");
+            obj.PrintPrime1();
+            Console.WriteLine("\n***********End of Question3***********\n");
+        }
+    }
+}
