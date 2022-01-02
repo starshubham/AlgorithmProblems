4ca7f7c19138cd53518557ba62570d4c721cc099
Shubham Seth <shubhamsth1234@gmail.com>
2022-01-02 12:42:58 +0530

    Added bubble sort code

---
+++ BubbleSort.cs
@@ -0,0 +1,55 @@
+﻿using System;
+/*
+Bubble sort - Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements 
+if they are in wrong order.
+Example: 
+First Pass: 
+( 5 1 4 2 8 ) –> ( 1 5 4 2 8 ), Here, algorithm compares the first two elements, and swaps since 5 > 1. 
+( 1 5 4 2 8 ) –>  ( 1 4 5 2 8 ), Swap since 5 > 4 
+( 1 4 5 2 8 ) –>  ( 1 4 2 5 8 ), Swap since 5 > 2 
+( 1 4 2 5 8 ) –> ( 1 4 2 5 8 ), Now, since these elements are already in order (8 > 5), algorithm does not swap them.
+Second Pass: 
+( 1 4 2 5 8 ) –> ( 1 4 2 5 8 ) 
+( 1 4 2 5 8 ) –> ( 1 2 4 5 8 ), Swap since 4 > 2 
+( 1 2 4 5 8 ) –> ( 1 2 4 5 8 ) 
+( 1 2 4 5 8 ) –>  ( 1 2 4 5 8 ) 
+Now, the array is already sorted, but our algorithm does not know if it is completed. 
+The algorithm needs one whole pass without any swap to know it is sorted.
+Third Pass: 
+( 1 2 4 5 8 ) –> ( 1 2 4 5 8 ) 
+( 1 2 4 5 8 ) –> ( 1 2 4 5 8 ) 
+( 1 2 4 5 8 ) –> ( 1 2 4 5 8 ) 
+( 1 2 4 5 8 ) –> ( 1 2 4 5 8 ) 
+*/
+
+namespace SortingInCSharp
+{
+    public class BubbleSort
+    {
+        public void bubbleSort(int[] arr1)
+        {
+            int n = arr1.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr1[j] > arr1[j + 1])
+                    {
+                        // swap temp and arr[i]
+                        int temp = arr1[j];
+                        arr1[j] = arr1[j + 1];
+                        arr1[j + 1] = temp;
+                    }
+                }                    
+            }                
+        }
+        /* Prints the array */
+        public void printArray1(int[] arr1)
+        {
+            int n = arr1.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr1[i] + " ");
+            Console.WriteLine();
+        }
+    }
+}
