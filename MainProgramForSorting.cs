93f8037e0cd77eccb1e16762012fccf64dee0a4a
Shubham Seth <shubhamsth1234@gmail.com>
2022-01-02 12:38:15 +0530

    Added Insertion sort program

---
+++ MainProgramForSorting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SortingInCSharp
+{
+    class MainProgramForSorting
+    {
+        static void Main(string[] args)
+        {
+            //InsertionSort
+            int[] arr = { 12, 11, 13, 5, 6 };
+            Console.WriteLine("Welcome to Insertion sort. Sorted array is: ");
+            InsertionSort ob = new InsertionSort();
+            ob.sort(arr);
+
+        }
+    }
+}
